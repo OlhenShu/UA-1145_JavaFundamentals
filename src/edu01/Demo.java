4cb59e94e8006a928f2e8e99443b7f2a517c9d3d
OlhenShu <olya.shutileva@gmail.com>
2024-02-26 20:01:59 +0200

    edu01

---
+++ src/edu01/Demo.java
@@ -0,0 +1,12 @@
+package edu01;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        byte x = 12;
+        byte y = 13;
+        byte res = (byte) (x + y);
+        System.out.println(res);
+    }
+}
