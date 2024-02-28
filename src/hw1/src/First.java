8e0e9acd63ea710fa5a5ec439163ed23f3dae167
fallestlaww <qtensell1@gmail.com>
2024-02-28 17:49:32 +0200

    first task fix

--- src/hw1/src/First.java
+++ src/hw1/src/First.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class First {
 
     public static void main(String[] args) {
-        double pi = 3.14;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the circle radius: ");
         int radius = scanner.nextInt();
-        double perimeter = (double)(2 * radius) * pi;
+        double perimeter = (double)(2 * radius) * Math.PI;
         System.out.println("Circle perimeter equals: " + perimeter);
-        double area = pi * (double)(radius * radius);
+        double area = Math.PI * (double)(radius * radius);
         System.out.println("Circle area equals: " + area);
         scanner.close();
     }
