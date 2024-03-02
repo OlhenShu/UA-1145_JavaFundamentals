10dbab6b9310f75831243c431a3696988a3c5821
Anna Vetrova <possiblyann@gmail.com>
2024-03-03 01:29:14 +0200

    Homework 1

---
+++ HW1/CallsCost.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int cost1, cost2, cost3; // Cost of each call
+        int time1, time2, time3; // Duration of each call
+        int totalCost; // Total cost of calls
+	int call1Cost, call2Cost, call3Cost; // Cost of each call based on duration
+        
+	Scanner scanner = new Scanner(System.in);
+        
+	System.out.println("Input cost and duration for the first call:");
+        cost1 = scanner.nextInt();
+        time1 = scanner.nextInt();
+        
+        System.out.println("Input cost and duration for the second call:");
+        cost2 = scanner.nextInt();
+        time2 = scanner.nextInt();
+        
+        System.out.println("Input cost and duration for the third call:");
+        cost3 = scanner.nextInt();
+        time3 = scanner.nextInt();
+        
+        scanner.close();
+
+	// Calculate the cost of each call based on duration
+        call1Cost = cost1 * time1;
+        call2Cost = cost2 * time2;
+        call3Cost = cost3 * time3;
+        
+        // Calculate the total cost of calls
+        totalCost = call1Cost + call2Cost + call3Cost;
+        
+        // Output the results
+        System.out.println("The cost of the first phone call for " + time1 + " minutes: $" + call1Cost);
+        System.out.println("The cost of the second phone call for " + time2 + " minutes: $" + call2Cost);
+        System.out.println("The cost of the third phone call for " + time3 + " minutes: $" + call3Cost);
+        System.out.println("Total cost: $" + totalCost);
+    }
+}
---
+++ HW1/FlowerBed.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        float radius;
+        float perimeter;
+        float area;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the radius of the flower bed:");
+        radius = scanner.nextFloat();
+        scanner.close();
+
+        perimeter = (float) (2*Math.PI*radius);
+        area = (float) (Math.PI*Math.pow(radius,2));
+
+        System.out.println("Perimeter of the flower bed: " + perimeter);
+        System.out.println("Area of the flower bed: " + area);
+    }
+}
