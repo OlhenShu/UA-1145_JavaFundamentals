f238ec99762f75d478b624ec5a390213d43e245a
possiblyann@gmail.com <possiblyann@gmail.com>
2024-03-04 22:00:22 +0200

    Homework is changed

---
+++ src/HW1/NameAddress.java
@@ -0,0 +1,22 @@
+package HW1;
+
+import java.util.Scanner;
+
+public class NameAddress {
+    public static void main(String[] args) {
+        String name;
+	String address;
+        
+	Scanner scanner = new Scanner(System.in);
+        
+	System.out.println("What is your name?");
+        name = scanner.nextLine();
+        
+	System.out.println("Where do you live, " + name + "?");
+        address = scanner.nextLine();
+	
+	scanner.close();
+        
+	System.out.println("Name: " + name + '\n' + "Adress: " + address);
+    }
+}
