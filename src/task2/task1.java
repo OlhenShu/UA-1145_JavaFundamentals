e6b32014f3d8de156151b166b8b506da1f92a296
Olesia <olesialytvynenko29@gmail.com>
2024-03-05 22:09:56 +0100

    Renme file

--- src/task2/task1.java
+++ src/task2/task1.java
@@ -1,25 +1,25 @@
-import java.util.Scanner;
-
-public class Task1 {
-    static int sidearr[] = new int[3];
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input first side:");
-        sidearr[0] = Integer.parseInt(scanner.nextLine());
-        System.out.println("Input second side:");
-        sidearr[1] = Integer.parseInt(scanner.nextLine());
-        System.out.println("Input third side:");
-        sidearr[2] = Integer.parseInt(scanner.nextLine());
-
-        int perimeter = calculatePerimeter();
-        System.out.println("Perimeter of the triangle: " + perimeter);
-    }
-
-    static int calculatePerimeter() {
-        int sum = 0;
-        for (int i = 0; i < 3; i++)
-            sum = sum + sidearr[i];
-        return sum;
-    }
-}
+//import java.util.Scanner;
+//
+//public class Task1 {
+//    static int sidearr[] = new int[3];
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Input first side:");
+//        sidearr[0] = Integer.parseInt(scanner.nextLine());
+//        System.out.println("Input second side:");
+//        sidearr[1] = Integer.parseInt(scanner.nextLine());
+//        System.out.println("Input third side:");
+//        sidearr[2] = Integer.parseInt(scanner.nextLine());
+//
+//        int perimeter = calculatePerimeter();
+//        System.out.println("Perimeter of the triangle: " + perimeter);
+//    }
+//
+//    static int calculatePerimeter() {
+//        int sum = 0;
+//        for (int i = 0; i < 3; i++)
+//            sum = sum + sidearr[i];
+//        return sum;
+//    }
+//}
