8a1bc821d2b0a141ee36695b6f70b0a0c1b4e1b7
Olesia <olesialytvynenko29@gmail.com>
2024-03-08 19:04:16 +0100

    Add task 2

--- src/task3/httpError.java
+++ src/task3/httpError.java
@@ -3,6 +3,7 @@ package task3;
 import java.util.Scanner;
 
 public class httpError {
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public enum HTTPError {
         ERROR_400("Bad Request"),
@@ -24,27 +25,45 @@ public class httpError {
         ERROR_416("Range Not Satisfiable"),
         ERROR_417("Expectation Failed");
 
-        private String description;
+        public final String description;
 
         HTTPError(String description) {
             this.description = description;
         }
 
-        public String getDescription() {
-            return description;
+        public static String getDescription(int number) {
+            return switch (number) {
+                case 400 -> ERROR_400.description;
+                case 401 -> ERROR_401.description;
+                case 402 -> ERROR_402.description;
+                case 403 -> ERROR_403.description;
+                case 404 -> ERROR_404.description;
+                case 405 -> ERROR_405.description;
+                case 406 -> ERROR_406.description;
+                case 407 -> ERROR_407.description;
+                case 408 -> ERROR_408.description;
+                case 409 -> ERROR_409.description;
+                case 410 -> ERROR_410.description;
+                case 411 -> ERROR_411.description;
+                case 412 -> ERROR_412.description;
+                case 413 -> ERROR_413.description;
+                case 414 -> ERROR_414.description;
+                case 415 -> ERROR_415.description;
+                case 416 -> ERROR_416.description;
+                case 417 -> ERROR_417.description;
+                default -> "Unknown Error";
+            };
         }
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Please, input a number representing an HTTP error:");
-        int inputErrorCode = Integer.parseInt(scanner.nextLine());
-        HTTPError[] errors = HTTPError.values();
-        HTTPError enumDescription = errors[inputErrorCode - 400];
-        String description = enumDescription.getDescription();
+        int inputErrorCode = Integer.parseInt(SCANNER.nextLine());
+        String description = HTTPError.getDescription(inputErrorCode);
 
         System.out.println("An HTTP error is: " + description);
 
-        scanner.close();
+        SCANNER.close();
     }
 }
+
