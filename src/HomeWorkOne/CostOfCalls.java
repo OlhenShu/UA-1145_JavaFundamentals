0c081f7fa5e1aaa5fb5522375281d366cd0d8e14
OlhenShu <olya.shutileva@gmail.com>
2024-03-11 15:55:58 +0200

    add some comments edu 01

--- src/HomeWorkOne/CostOfCalls.java
+++ src/HomeWorkOne/CostOfCalls.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CostOfCalls {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введіть тривалість 1 дзвінка: ");
+        System.out.println("Введіть тривалість 1 дзвінка: ");//be careful with the language, it's better to use English
         int time1 = scanner.nextInt();
         System.out.println("Введіть ціну за хвилину 1 дзвінка ");
         int cost1 = scanner.nextInt();
@@ -23,7 +23,7 @@ public class CostOfCalls {
         int sum1 = time1 * cost1;
         int sum2 = time2 * cost2;
         int sum3 = time3 * cost3;
-        int totalsum = sum1 + sum2 + sum3;
+        int totalsum = sum1 + sum2 + sum3;//it's better to use camelCase for the variable name (totalSum)
 
         System.out.println("Вартість 1 дзвінка: " + sum1);
         System.out.println("Вартість 2 дзвінка: " + sum2);
