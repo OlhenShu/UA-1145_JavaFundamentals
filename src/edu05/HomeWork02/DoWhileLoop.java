50759b8364f133ebf97395ecd6d23b22ddcbb5cf
localbuddha <localbuddha@gmail.com>
2024-03-18 14:45:22 +0200

    HW01 was added into EDU05

---
+++ src/edu05/HomeWork02/DoWhileLoop.java
@@ -0,0 +1,33 @@
+package edu05.HomeWork02;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(in);
+        int[] twoNumbers = new int[2];
+        String userResp;
+
+        do {
+            System.out.println("Введіть 2 числа: ");
+            for (int i = 0; i < twoNumbers.length; i++) {
+                twoNumbers[i] = scan.nextInt();
+            }
+            System.out.println("The sum is:" + sumCalc(twoNumbers));
+
+            System.out.print("Може, ще разочок? (yes/no): ");
+            userResp = scan.next();
+        }
+        while (userResp.equalsIgnoreCase("yes"));
+    }
+
+    private static int sumCalc(int[] twoNumbers) {
+        int sum = 0;
+        for (int twoNumber : twoNumbers) {
+            sum += twoNumber;
+        }
+        return sum;
+    }
+}
