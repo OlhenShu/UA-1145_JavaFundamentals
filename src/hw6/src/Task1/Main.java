6dfcea77509df9f7544fc3c1f8f03f3c5cf48502
fallestlaww <qtensell1@gmail.com>
2024-03-19 19:58:29 +0200

    fix v1

--- src/hw6/src/Task1/Main.java
+++ src/hw6/src/Task1/Main.java
@@ -2,19 +2,14 @@ package Task1;
 
 public class Main {
     public static void main(String[] args) {
-        Eagle eagle = new Eagle("Contoured", true);
-        Swallow swallow = new Swallow("Filamentous", true);
-        Penguin penguin = new Penguin("Downy", true);
-        Kiwi kiwi = new Kiwi("Downy", true);
+       Bird[] bird = {  new Eagle("Contoured", true),
+               new Swallow("Filamentous", true),
+               new Penguin("Downy", true),
+               new Kiwi("Downy", true) };
 
-        eagle.fly();
-        swallow.fly();
-        penguin.fly();
-        kiwi.fly();
-
-        eagle.output();
-        swallow.output();
-        penguin.output();
-        kiwi.output();
+        for (int i = 0; i < bird.length; i++) {
+            bird[i].output();
+            bird[i].fly();
+        }
     }
 }
