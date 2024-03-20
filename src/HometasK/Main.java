c29afb63a04d29ab1f8c4c738e93c5bf20c6ad64
Iryna Pavlyk <iruska.m1@ukr.net>
2024-03-20 12:48:45 +0200

    update branch dev

---
+++ src/HometasK/Main.java
@@ -0,0 +1,16 @@
+
+package HometasK;
+import static Practical.Practical_1.*;
+import static HometasK.Homework_1.*;
+public class Main {
+
+    public static void main(String[] args) {
+        task_1();
+        task_2();
+        task_3();
+        practical_1_Task_1();
+        practical_1_Task_2();
+        practical_1_Task_3();
+        practical_1_Task_4();
+    }
+}
