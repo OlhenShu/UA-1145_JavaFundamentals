7114454f5ff5d078673c730e4243d500ef30206b
kiborgozavr <k.volianiuk@gmail.com>
2024-03-20 21:28:19 +0200

    formated

--- src/Practicals/Pr5/Ex4/Employee.java
+++ src/Practicals/Pr5/Ex4/Employee.java
@@ -13,26 +13,26 @@ public class Employee {
         this.salary = salary;
     }
 
-    public static ArrayList <Employee> sortByTheSalary (ArrayList <Employee> employeeArrayList){
-        for (int i = 0; i < employeeArrayList.size()-1; i++) {
+    public static ArrayList<Employee> sortByTheSalary(ArrayList<Employee> employeeArrayList) {
+        for (int i = 0; i < employeeArrayList.size() - 1; i++) {
             Employee temp1 = employeeArrayList.get(i);
-            Employee temp2 = employeeArrayList.get(i+1);
-            if (temp1.getSalary() < temp2.getSalary()){
+            Employee temp2 = employeeArrayList.get(i + 1);
+            if (temp1.getSalary() < temp2.getSalary()) {
                 Employee temp = temp1;
                 employeeArrayList.remove(i);
-                employeeArrayList.add((i+1), temp);
+                employeeArrayList.add((i + 1), temp);
                 i = 0;
             }
         }
         return employeeArrayList;
     }
 
-    public static ArrayList <String> findWorkersOfDepartment (ArrayList <Employee> employeeArrayList, int departmentNumber){
-        ArrayList <String> resultList = new ArrayList<>();
+    public static ArrayList<String> findWorkersOfDepartment(ArrayList<Employee> employeeArrayList, int departmentNumber) {
+        ArrayList<String> resultList = new ArrayList<>();
         for (int i = 0; i < employeeArrayList.size(); i++) {
             Employee temp = employeeArrayList.get(i);
             int depNumbOfTempEmployee = temp.getDepartmentNumber();
-            if (depNumbOfTempEmployee == departmentNumber){
+            if (depNumbOfTempEmployee == departmentNumber) {
                 resultList.add(temp.getName());
             }
         }
@@ -40,26 +40,32 @@ public class Employee {
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public int getDepartmentNumber() {
+
         return departmentNumber;
     }
 
     public void setDepartmentNumber(int departmentNumber) {
+
         this.departmentNumber = departmentNumber;
     }
 
     public int getSalary() {
+
         return salary;
     }
 
     public void setSalary(int salary) {
+
         this.salary = salary;
     }
 }
