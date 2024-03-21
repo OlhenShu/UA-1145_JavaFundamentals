53fb93def400736bb29a5bd5524677f0b465c615
possiblyann@gmail.com <possiblyann@gmail.com>
2024-03-21 21:40:52 +0200

    edu07

--- src/edu07/monthlywage/Main.java
+++ src/edu07/monthlywage/Main.java
@@ -2,12 +2,12 @@ package edu07.monthlywage;
 
 public class Main {
     public static void main(String[] args) {
-        Employee[] empls = {
+        Employee[] employees = {
                 new ContractEmployee("123-767", "Josephina", "1234-675"),
                 new SalariedEmployee("3478632-34578654", "Vasilii", "1234", 124),
                 new SalariedEmployee("1234", "Djigan", "1234567", 72)};
 
-        for (Employee object : empls) {
+        for (Employee object : employees) {
             if (object instanceof ContractEmployee) {
                 System.out.print("Employee name: " + object.getName() + "\nEmployee ID: " + object.getEmployeeId() + "\nFederal number: " + ((ContractEmployee) object).getFederalTaxIdMember() + "\nAverage monthly wage: ");
                 ((ContractEmployee) object).calculatePay();
