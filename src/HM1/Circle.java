a7b10934950f497fdf77c303fda300ed49f0467e
OlhenShu <olya.shutileva@gmail.com>
2024-03-24 22:22:36 +0200

    add some comments edu 01-03

--- src/HM1/Circle.java
+++ src/HM1/Circle.java
@@ -8,8 +8,8 @@ public class Circle {
         System.out.println("Введіть радіус: ");
         Scanner sc = new Scanner(System.in);
         radius = sc.nextInt();
-        float Pi = 3.14f;
-        float areaCircle =  Pi * radius * radius;
+        float Pi = 3.14f;//use Math.PI
+        float areaCircle = Pi * radius * radius;// here can be Math.PI * Math.pow(radius, 2);
         float lengthCircle = 2 * Pi * radius;
         System.out.println("Довжина кола: " + lengthCircle);
         System.out.println("Площа кола: " + areaCircle);
--- src/HM2/triangleArea.java
+++ src/HM2/triangleArea.java
@@ -2,17 +2,22 @@ package HM2;
 
 import java.util.Scanner;
 
-public class triangleArea {
+public class triangleArea {//TriangleArea
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input side of triangle: ");
-        int side= sc.nextInt();
+        int side = sc.nextInt();//better variable name base
         System.out.print("Input high of triangle: ");
-        int high = sc.nextInt();
-        double area =getArea(side,high);
-        System.out.println("Area of triangle" + area);
+        int high = sc.nextInt();//better variable name height
+        double area = getArea(side, high);
+        System.out.println("Area of the triangle: " + area);
     }
-    public static double getArea(int side , int high){
-        return 0.5f * side * high;
+
+    public static double getArea(int side, int high) {
+        return 0.5f * side * high;//why do you use float?
     }
+//    public static double calculateArea(int base, int height) {
+//        return 0.5 * base * height;
+//    }
 }
