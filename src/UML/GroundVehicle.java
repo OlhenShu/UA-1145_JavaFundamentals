5fe03719b99a950b6a66341d34a3409dc973941f
Olesia Lytvynenko <124312255+Olesialyt@users.noreply.github.com>
2024-03-27 12:54:52 +0100

    Delete src/UML directory

--- src/UML/GroundVehicle.java
+++
@@ -1,9 +0,0 @@
-package UML;
-
-public abstract class GroundVehicle extends Passengers implements Vehicle{
-
-    public GroundVehicle(int passengers) {
-        super(passengers);
-    }
-    abstract void drive();
-}
--- src/UML/Vehicle.java
+++
@@ -1,4 +0,0 @@
-package UML;
-
-public interface Vehicle {
-}
