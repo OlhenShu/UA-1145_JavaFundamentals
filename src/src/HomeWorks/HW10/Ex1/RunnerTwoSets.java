297bd481e6cc0a6d9085538ab8bd40462d3c720b
OlhenShu <olya.shutileva@gmail.com>
2024-05-02 12:50:06 +0300

    added comments to HW10

--- src/src/HomeWorks/HW10/Ex1/RunnerTwoSets.java
+++ src/src/HomeWorks/HW10/Ex1/RunnerTwoSets.java
@@ -8,6 +8,9 @@ public class RunnerTwoSets {
     public static void main(String[] args) {
         Set<Integer> set1 = new HashSet<>();
         Set<Integer> set2 = new HashSet<>();
+        //For small task like this, make sense use Set.of
+//        Set<Integer> set1 = Set.of(2, 4, 6, 8, 10, 12);
+//        Set<Integer> set2 = Set.of(1, 2, 3, 4, 5, 6);
         set1.add(2);
         set1.add(4);
         set1.add(6);
@@ -43,4 +46,21 @@ public class RunnerTwoSets {
         }
         return newSet;
     }
+    //The intersection method can be made more efficient by checking which set is smaller and iterating over it,
+    // which reduces the number of containment checks
+
+    //It's minimize the number of contains calls
+    // by always iterating over the smaller set and checking against the larger one.
 }
+//public static Set<Integer> intersectSets(Set<Integer> set1, Set<Integer> set2) {
+//    Set<Integer> smaller = set1.size() <= set2.size() ? set1 : set2;
+//    Set<Integer> larger = set1.size() > set2.size() ? set1 : set2;
+//
+//    Set<Integer> newSet = new HashSet<>();
+//    for (Integer i : smaller) {
+//        if (larger.contains(i)) {
+//            newSet.add(i);
+//        }
+//    }
+//    return newSet;
+//}
