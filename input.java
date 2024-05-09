4c5cbeb24aacb8f48d77ec162d9750d8c0a9d557
possiblyann@gmail.com <possiblyann@gmail.com>
2024-05-09 22:28:57 +0300

    edu16

--- input.java
+++ input.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class Task {
+public class Modifire {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
--- output.java
+++ output.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-private class Task {
+private class Modifire {
     private static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
