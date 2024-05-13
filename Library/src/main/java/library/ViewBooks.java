898d9284403f643dedf170b119d91549bf0f31bf
Hryhorii-Shtanko <112967804+Hryhorii-Shtanko@users.noreply.github.com>
2024-05-13 16:13:45 +0300

    Update ViewBooks.java
    add printf to better view

--- Library/src/main/java/library/ViewBooks.java
+++ Library/src/main/java/library/ViewBooks.java
@@ -4,16 +4,12 @@ import java.util.ArrayList;
 
 public class ViewBooks implements Operation {
 
-    @Override
-    public void operate(DataBase database, User user) {
-        ArrayList<Book> books = database.getAllBooks();
-        System.out.println("Book Name\t\t\t AuthorOfBook\t\t\t Publisher \t\tPrice \t\tQuantity");
+     ArrayList<Book> books = database.getAllBooks();
+        System.out.printf("%-30s %-30s %-30s %-10s %-10s%n", "Book Name", "AuthorOfBook", "Publisher", "Price", "Quantity");
         for (Book b : books) {
-            System.out.println(b.getName() + "\t\t" + b.getAuthor() +
-                    "\t\t\t\t" + b.getPublisher() + "\t\t\t" + b.getPrice() + "\t\t\t" + b.getQuantity());
+            System.out.printf("%-30s %-30s %-30s %-10.2f %-10d%n", b.getName(), b.getAuthor(), b.getPublisher(), b.getPrice(), b.getQuantity());
         }
         System.out.println();
         user.menu(database, user);
-
     }
 }
